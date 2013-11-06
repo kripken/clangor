e34a24315ccedb95750f78ce2e943d171986ab1d
Alon Zakai <alonzakai@gmail.com>
2013-11-06 14:35:21 -0800

    work

--- build.sh
+++ build.sh
@@ -1,26 +1,26 @@
 #
 cd native
-../configure
+../configure --enable-optimized --disable-assertions --disable-jit --disable-threads --disable-pthreads --enable-targets=sparc
 make -j 4
 cd ..
 cd js
-~/Dev/emscripten/emconfigure ../configure --disable-assertions --enable-optimized
+~/Dev/emscripten/emconfigure ../configure --enable-optimized --disable-assertions --disable-jit --disable-threads --disable-pthreads --enable-targets=sparc
 make
 # copy native tblgen, and remove makefile command to build it from js so we don't replace the native one
-cp ../native/Debug+Asserts/bin/llvm-tblgen Release/bin
+cp ../native/Release/bin/llvm-tblgen Release/bin
 chmod +x Release/bin/llvm-tblgen
 cat utils/TableGen/Makefile | grep -v llvm-tblgen > utils/TableGen/Makefile # XXX untested
 
-cp ../native/Debug+Asserts/bin/llvm-config Release/bin
+cp ../native/Release/bin/llvm-config Release/bin
 chmod +x Release/bin/llvm-config
 cat tools/llvm-config/Makefile | grep -v TOOLNAME > tools/llvm-config/Makefile # XXX untested
 
-cp ../native/Debug+Asserts/bin/clang-tblgen Release/bin
+cp ../native/Release/bin/clang-tblgen Release/bin
 chmod +x Release/bin/clang-tblgen
 cat tools/clang/utils/TableGen/Makefile | grep -v clang-tblgen > tools/clang/utils/TableGen/Makefile # XXX untested
 
 cp Release/bin/clang clang.bc
-EMCC_DEBUG=1 ~/Dev/emscripten/emcc -O2 -g2 clang.bc -o clang.js -s ASM_JS=2 # -s OUTLINING_LIMIT=80000
+EMCC_DEBUG=1 ~/Dev/emscripten/emcc -O2 clang.bc -o clang.js -s ASM_JS=2 -s OUTLINING_LIMIT=110000
 
 # see js/test.html
 
